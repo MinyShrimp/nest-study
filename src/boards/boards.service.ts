b3ef967e290fad7c6656d1aebd5763a2f26b972c
MinyShrimp <ksk7584@gmail.com>
2022-04-27 17:53:48 +0900

    create board model

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BoardsService {
-    private boards = [];
+    private boards = [ "안녕" ];
 
     getAllBoards() {
         return this.boards;
