7aea2ef8cf3adbd3129e7f2a3813fcfa941ac5f0
MinyShrimp <ksk7584@gmail.com>
2022-04-28 22:34:03 +0900

    add auth in board controller

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -7,6 +7,7 @@ import {
     ParseIntPipe,
     Patch,
     Post,
+    UseGuards,
     UsePipes,
     ValidationPipe,
 } from '@nestjs/common';
@@ -16,8 +17,10 @@ import { BoardsService } from './boards.service';
 import { CreateBoardDTO } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 import { DeleteResult } from 'typeorm';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('boards')
+@UseGuards(AuthGuard())
 export class BoardsController {
     constructor(private readonly boardsService: BoardsService) {}
 
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { DatabaseModule } from 'src/database/database.module';
 import { BoardsController } from './boards.controller';
 import { boardsProviders } from './boards.providers';
 import { BoardsService } from './boards.service';
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, AuthModule],
     controllers: [BoardsController],
     providers: [...boardsProviders, BoardsService],
 })
