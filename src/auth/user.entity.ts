4582f143d97e905982261179ead13b8fef3d7734
MinyShrimp <ksk7584@gmail.com>
2022-04-28 22:39:32 +0900

    add relationship user and board

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,6 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { Board } from "src/boards/boards.entity";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
+import * as bcrypt from "bcryptjs";
 
 @Entity()
 @Unique(['name'])
@@ -14,4 +16,12 @@ export class User extends BaseEntity {
 
     @Column()
     salt: string;
+
+    @OneToMany(type => Board, board => board.user, { eager: true })
+    boards: Board[];
+
+    async validatePassword(password: string) {
+        const isValid = await bcrypt.compare(password, this.password);
+        return isValid;
+    }
 }
